9090eaabf7f2f8412baa54d5455376dc68cd6009
xhlar <contacto@xhlar.com>
2022-09-25 18:21:48 -0500

    Update homepage and user view
    Signed-off-by: xhlar <contacto@xhlar.com>

--- views/user.py
+++ views/user.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import logout_user, login_required
 from models import Producto
 
